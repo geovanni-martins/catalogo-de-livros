7c9f530c2b730d5b7252c0c76f217927e654b96d
Geovanni <geovs.dev@gmail.com>
2025-06-19 12:04:19 -0300

    aside disponível em todos tamanhos de tela

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-function mostrarAdicaoLivros() {
+function mostrarAdicaoLivrosTelasPequenas() {
     const aside = document.getElementsByTagName("aside")[0]
     if (aside.style.display == "none") {
         aside.style.display = "block"
     } else {
         aside.style.display = "none"
     }
-}   
+}  
+
+function mostrarAdicaoLivrosTelasMaiores() {
+    let setaRight = document.getElementById("setaRight")
+    let setaLeft = document.getElementById("setaLeft")
+    const aside = document.getElementsByTagName("aside")[0]
+    
+    if (aside.style.display == "none") {
+        aside.style.display = "block"
+        setaRight.style.display = "none"
+        setaLeft.style.display = "block"
+    } else {
+        aside.style.display = "none"
+        setaLeft.style.display = "none"
+        setaRight.style.display = "block"
+    }
+}
